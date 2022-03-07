499cb53579dfdfabf8d874f4f5d24f4ff0e16c3d
bruno-costanzo <dev.bcostanzo@gmail.com>
2022-03-07 20:17:49 -0300

    Initial

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :posts, only: %i[new create index]
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+  root 'posts#index'
+end
